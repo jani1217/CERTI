0e58388e5bbd909c632f2747b1c22c5a26ac3699
jani1217 <125665133+jani1217@users.noreply.github.com>
2024-03-03 11:30:35 +0530

    comm

--- certi/src/App.jsx
+++ certi/src/App.jsx
@@ -1,23 +1,17 @@
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/navbar';
 import Login from './components/login';
-import CertificateGenerator from './components/CertificateGenerator';
+import CertificateGenerator from './components/CertificateGenerator';//import Footer from './components/footer'; // Import the ExcelFileUpload component
+import Navbar from './components/navbar';
 import ImageSlider from './components/ImageSlider';
-
 const App = () => {
     return (
-        <Router>
-            <div className="app">
-                <Navbar />
-                <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route path="/image-slider" component={ImageSlider} />
-                    <Route path="/certificate-generator" component={CertificateGenerator} />
-                    {/* Add more routes here */}
-                </Switch>
-            </div>
-        </Router>
+    
+        <div className="app">
+            <Navbar/>
+            <Login />
+            <ImageSlider/>
+            <CertificateGenerator />
+        </div>
+    
     );
 };
 
